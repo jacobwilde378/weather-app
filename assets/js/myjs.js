f1e962f241bcf939d9b97fedd07c9e1ef87ded0f
Jacob.Wilde <jacob.wilde@utah.edu>
2020-09-05 16:57:42 -0600

    flexbox issues

--- assets/js/myjs.js
+++ assets/js/myjs.js
@@ -1,4 +1,8 @@
 var api_key = "c49acaab6fd47abd921dc6918d8cf53b"
+var convertKelvin = function (kTemp) {
+   var fTemp = Math.round(((kTemp - 273.15) * (9/5) + 32))
+   return fTemp
+}
 function weatherSearch() {
     var cityName = document.querySelector("#searchCityName").value;
     console.log(cityName)
@@ -12,5 +16,24 @@ function weatherSearch() {
     })
     .then(function(response) {
         console.log(response)
+        var lat = response.city.coord.lat;
+        var lon = response.city.coord.lon;
+        return fetch(
+            'https://api.openweathermap.org/data/2.5/onecall?'
+            + 'lat=' + lat
+            + '&lon=' + lon
+            + '&appid=' + api_key
+        );
+    })
+    .then(function(formatData) {
+        return formatData.json();
+    })
+    .then(function(weatherData) {
+        console.log(weatherData)
+        var currentTemp = convertKelvin(weatherData.current.temp)
+        var currentHumidity = weatherData.current.humidity
+        var currentWindSpeed = weatherData.current.wind_speed
+        var currentUV = weatherData.current.uvi
+        console.log(currentTemp, currentHumidity, currentWindSpeed, currentUV)
     })
 }
