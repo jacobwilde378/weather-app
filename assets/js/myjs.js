85ecd87220b74cbc1756505416be2c9c0387007f
Jacob.Wilde <jacob.wilde@utah.edu>
2020-09-05 23:32:37 -0600

    add current weather display div

--- assets/js/myjs.js
+++ assets/js/myjs.js
@@ -18,6 +18,8 @@ function weatherSearch() {
         console.log(response)
         var lat = response.city.coord.lat;
         var lon = response.city.coord.lon;
+        var name = response.city.name
+        $("#displayCityName").text(name)
         return fetch(
             'https://api.openweathermap.org/data/2.5/onecall?'
             + 'lat=' + lat
@@ -34,6 +36,34 @@ function weatherSearch() {
         var currentHumidity = weatherData.current.humidity
         var currentWindSpeed = weatherData.current.wind_speed
         var currentUV = weatherData.current.uvi
+        var currentIconLink = 'http://openweathermap.org/img/wn/' + weatherData.current.weather[0].icon + '@2x.png'
+        var currentIconAlt = weatherData.current.weather[0].main
         console.log(currentTemp, currentHumidity, currentWindSpeed, currentUV)
+        $("#curIcon").attr("src", currentIconLink)
+        $("#curIcon").attr("alt", currentIconAlt)
+        $("#curTemp").text(currentTemp + String.fromCharCode(8457))
+        $("#curHumidity").text(currentHumidity + "%")
+        $("#curWindSpeed").text(currentWindSpeed + " MPH")
+        $("#curUV").text(currentUV)
+        var uvBox = $(".shade")
+        uvBox.removeClass("favorable moderate severe")
+        if (currentUV <= 2) {
+            uvBox.addClass("favorable")
+        }
+        else if(currentUV <= 5) {
+            uvBox.addClass("moderate")
+        }
+        else {
+            uvBox.addClass("severe")
+        }
+        var day1 = moment()
+        console.log(day1.format())
+
+
+
+
+
+
+        console.log("done")
     })
 }
